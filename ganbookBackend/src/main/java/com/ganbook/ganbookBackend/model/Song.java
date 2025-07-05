a94ef229d7002ddfbcde60c64bd2a1e86167125f
subhadipm9 <164364117+subhadipm9@users.noreply.github.com>
2025-07-06 01:09:21 +0530

    Added getters and setters in Song class

--- ganbookBackend/src/main/java/com/ganbook/ganbookBackend/model/Song.java
+++ ganbookBackend/src/main/java/com/ganbook/ganbookBackend/model/Song.java
@@ -1,10 +1,112 @@
 package com.ganbook.ganbookBackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long songID;
     private String songName;
     private float songLength;
     private String songURL;
     private String singerName;
+    private String genre;
+    private boolean isFavorite;
+    private String thumbnailUrl;
+    private LocalDateTime addedOn;
 
+    public long getSongID() {
+        return songID;
+    }
+
+    public void setSongID(long songID) {
+        this.songID = songID;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public float getSongLength() {
+        return songLength;
+    }
+
+    public void setSongLength(float songLength) {
+        this.songLength = songLength;
+    }
+
+    public String getSongURL() {
+        return songURL;
+    }
+
+    public void setSongURL(String songURL) {
+        this.songURL = songURL;
+    }
+
+    public String getSingerName() {
+        return singerName;
+    }
+
+    public void setSingerName(String singerName) {
+        this.singerName = singerName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public LocalDateTime getAddedOn() {
+        return addedOn;
+    }
+
+    public void setAddedOn(LocalDateTime addedOn) {
+        this.addedOn = addedOn;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "songID=" + songID +
+                ", songName='" + songName + '\'' +
+                ", songLength=" + songLength +
+                ", songURL='" + songURL + '\'' +
+                ", singerName='" + singerName + '\'' +
+                ", genre='" + genre + '\'' +
+                ", isFavorite=" + isFavorite +
+                ", thumbnailUrl='" + thumbnailUrl + '\'' +
+                ", addedOn=" + addedOn +
+                '}';
+    }
 }
