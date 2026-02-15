c9ab5f9be4a0191d0520d06e74697296a9af6445
subhadipm9 <164364117+subhadipm9@users.noreply.github.com>
2026-02-15 20:16:56 +0530

    can download the music but unable to push in drive. To help the code created the helper class

--- ganbookBackend/src/main/java/com/ganbook/ganbookBackend/helper/GoogleDriveUploader.java
+++ ganbookBackend/src/main/java/com/ganbook/ganbookBackend/helper/GoogleDriveUploader.java
@@ -1,17 +1,30 @@
 package com.ganbook.ganbookBackend.helper;
 
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import org.apache.http.auth.Credentials;
+//import com.google.api.client.json.JsonFactory;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+
+import javax.imageio.spi.IIORegistry;
+import java.io.*;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
-
+@Deprecated
 public class GoogleDriveUploader {
-    public class UploadToDrive {
+    public String authenticateToGoogleDrive() throws IOException, GeneralSecurityException {
+        var credentials = GoogleCredential.fromStream(new FileInputStream("credentials.json"))
+        .createScoped(Collections.singleton("https://www.googleapis.com/auth/drive"));
 
-        }
+//        var driveService = new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.grt);
+        return null;
+    }
 }
 
